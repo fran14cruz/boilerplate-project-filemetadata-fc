ca51bf4ee28a5684de1f0a4586684b36e87fc69e
Francisco Cruz <fran14.ich@gmail.com>
2023-03-06 23:52:46 -0500

    Adding logic for file upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+// require Multer to handle file upload
 const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -12,8 +14,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// do post to /api/fileanalyse route
+// upload.single('FILE_NAME') -> file name in html input tag
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
